41ecab8d5eb48133151fe5a32518d28593d1dbce
bornleader10 <a143323218@gmail.com>
2024-11-04 22:15:27 +0800

    Site updated: 2024-11-04 22:15:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/02/hello-world/","2024/11/03/生活/","2024/11/03/科研/","2024/11/03/算法/"];function toRandomPost(){
+var posts=["2024/11/02/hello-world/","2024/11/04/Cpp-unit06/","2024/11/03/生活/","2024/11/03/科研/","2024/11/03/算法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
