073a21d422249a89c72b7f4b423711e2778036ca
bornleader10 <a143323218@gmail.com>
2024-11-05 19:41:04 +0800

    Site updated: 2024-11-05 19:41:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/04/Cpp-unit06/","2024/11/05/planA/","2024/11/02/hello-world/","2024/11/03/生活/","2024/11/03/科研/"];function toRandomPost(){
+var posts=["2024/11/04/Cpp-unit06/","2024/11/02/hello-world/","2024/11/05/planA/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
