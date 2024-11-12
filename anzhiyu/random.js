6f9ab949448a9c1d9aa7249171afac49012e7942
bornleader10 <a143323218@gmail.com>
2024-11-12 12:29:02 +0800

    Site updated: 2024-11-12 12:29:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/04/Cpp-unit06/","2024/11/05/planA/","2024/11/02/hello-world/","2024/11/05/《TCP-IP网络编程》第一章/","2024/11/10/计划书/","2024/11/06/指针/","2024/11/08/计算机基础/"];function toRandomPost(){
+var posts=["2024/11/12/会议记录/","2024/11/08/计算机基础/","2024/11/06/指针/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
